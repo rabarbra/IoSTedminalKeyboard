46fdcd7c9a481320ec29809783b2ab398958b384
Polina Simonenko <polina.simonenko@wobcom.de>
2024-11-01 20:25:48 +0100

    Initial Commit

---
+++ TerminalKeyboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TerminalKeyboard
+//
+//  Created by Polina Simonenko on 01.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TerminalKeyboard/TerminalKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminalKeyboardApp.swift
+//  TerminalKeyboard
+//
+//  Created by Polina Simonenko on 01.11.24.
+//
+
+import SwiftUI
+
+@main
+struct TerminalKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
